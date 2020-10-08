313b835b8419c84c4068adb1b403d45935348ffa
Michael Meuli <michael.meuli@gmail.com>
2020-10-08 16:08:48 +0200

    Initial commit

---
+++ calendar/index.js
@@ -0,0 +1,64 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const architect_1 = require("@angular-devkit/architect");
+const fetch = require("node-fetch");
+const fs = require('fs').promises;
+require('dotenv').config({ path: '../yoga-lichtquelle/.env' });
+const API_KEY = process.env.API_KEY;
+const googleCalendarId = process.env.GOOGLE_CALENDAR_ID;
+const API_BASE = 'https://www.googleapis.com/calendar/v3/calendars';
+const filepath = './src/assets/google-calendar.json';
+exports.default = architect_1.createBuilder((options, context) => {
+    return new Promise((resolve, reject) => {
+        context.reportStatus(`Executing "${options.command}"...`);
+        getJson()
+            .then(() => {
+            resolve({ success: true });
+        })
+            .catch(e => reject(e));
+        context.reportStatus(`Done.`);
+    });
+});
+function getJson() {
+    let url = new URL(buildUrl());
+    let params = { key: API_KEY };
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data["items"])
+        .then(items => gcalItemsToRawEventDefs(items))
+        .then(events => fs.writeFile(filepath, JSON.stringify(events)));
+}
+function buildUrl() {
+    return API_BASE + '/' + encodeURIComponent(parseGoogleCalendarId(googleCalendarId)) + '/events';
+}
+// https://github.com/fullcalendar/fullcalendar/blob/master/packages/google-calendar/src/main.ts
+function parseGoogleCalendarId(url) {
+    let match;
+    // detect if the ID was specified as a single string.
+    // will match calendars like "asdf1234@calendar.google.com" in addition to person email calendars.
+    if (/^[^/]+@([^/.]+\.)*(google|googlemail|gmail)\.com$/.test(url)) {
+        return url;
+    }
+    else if ((match = /^https:\/\/www.googleapis.com\/calendar\/v3\/calendars\/([^/]*)/.exec(url)) ||
+        (match = /^https?:\/\/www.google.com\/calendar\/feeds\/([^/]*)/.exec(url))) {
+        return decodeURIComponent(match[1]);
+    }
+}
+function gcalItemsToRawEventDefs(items) {
+    return items.map((item) => {
+        return gcalItemToRawEventDef(item);
+    });
+}
+function gcalItemToRawEventDef(item) {
+    return {
+        id: item.id,
+        title: item.summary,
+        start: item.start.dateTime || item.start.date,
+        end: item.end.dateTime || item.end.date,
+        url: item.htmlLink,
+        location: item.location,
+        description: item.description
+    };
+}
+//# sourceMappingURL=index.js.map
---
+++ calendar/index.ts
@@ -0,0 +1,86 @@
+import { BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+const fetch = require("node-fetch");
+const fs = require('fs').promises;
+
+require('dotenv').config({ path: '../yoga-lichtquelle/.env' })
+const API_KEY = process.env.API_KEY;
+const googleCalendarId = process.env.GOOGLE_CALENDAR_ID;
+const API_BASE = 'https://www.googleapis.com/calendar/v3/calendars'
+const filepath = './src/assets/google-calendar.json';
+
+
+
+interface Options extends JsonObject {
+    command: string;
+    args: string[];
+}
+
+export default createBuilder<Options>((options, context) => {
+    return new Promise<BuilderOutput>((resolve, reject) => {
+
+        context.reportStatus(`Executing "${options.command}"...`);
+
+        getJson()
+            .then(() => {
+                resolve({ success: true })
+            })
+            .catch(e => reject(e));
+
+        context.reportStatus(`Done.`);
+    });
+});
+
+
+function getJson() {
+    let url = new URL(buildUrl());
+    let params = { key: API_KEY };
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data["items"])
+        .then(items => gcalItemsToRawEventDefs(items))
+        .then(events => fs.writeFile(filepath, JSON.stringify(events)))
+}
+
+
+// https://github.com/fullcalendar/fullcalendar/blob/master/packages/google-calendar/src/main.ts
+
+function buildUrl() {
+    return API_BASE + '/' + encodeURIComponent(parseGoogleCalendarId(googleCalendarId)) + '/events'
+}
+
+function parseGoogleCalendarId(url) {
+    let match
+  
+    // detect if the ID was specified as a single string.
+    // will match calendars like "asdf1234@calendar.google.com" in addition to person email calendars.
+    if (/^[^/]+@([^/.]+\.)*(google|googlemail|gmail)\.com$/.test(url)) {
+        return url
+    } else if (
+        (match = /^https:\/\/www.googleapis.com\/calendar\/v3\/calendars\/([^/]*)/.exec(url)) ||
+        (match = /^https?:\/\/www.google.com\/calendar\/feeds\/([^/]*)/.exec(url))
+    ){
+        return decodeURIComponent(match[1])
+    }
+}
+
+function gcalItemsToRawEventDefs(items: any) {
+    return items.map((item: any) => {
+        return gcalItemToRawEventDef(item);
+    });
+}
+
+function gcalItemToRawEventDef(item: any) {
+    return {
+        id: item.id,
+        title: item.summary,
+        start: item.start.dateTime || item.start.date, // try timed. will fall back to all-day
+        end: item.end.dateTime || item.end.date, // same
+        url: item.htmlLink,
+        location: item.location,
+        description: item.description
+    }
+}
+
